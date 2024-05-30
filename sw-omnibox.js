659a3f045f3f348748da7fd92e18b7ffa1d6d861
andysama79 <arnab.dey@plaksha.edu.in>
2024-05-30 15:17:28 +0530

    minor

--- sw-omnibox.js
+++ sw-omnibox.js
@@ -1,7 +1,7 @@
 console.log("sw-omnibox.js")
 
 // save default API suggestions
-chrome.runtime.onInstalled.addListener(( { reason}) => {
+chrome.runtime.onInstalled.addListener(( { reason }) => {
     if (reason === 'install') {
         chrome.storage.local.set({
             apiSuggestions: ['tabs', 'storage', 'scripting']
